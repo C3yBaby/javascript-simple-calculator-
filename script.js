3c279f50a89220d1c442c9d82ba6e53885bc75bc
Helix Nebula <106392013+C3yBaby@users.noreply.github.com>
2022-07-17 18:01:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function AllDel(){
+    document.getElementById("result"). value = ""
+
+}
+
+function Calculate(num1){
+    document.getElementById("result"). value += num1;
+}
+function Equle(){
+ var a = document.getElementById("result"). value;
+ var b = eval(a)
+ var a = document.getElementById("result"). value = b;
+
+
+}
